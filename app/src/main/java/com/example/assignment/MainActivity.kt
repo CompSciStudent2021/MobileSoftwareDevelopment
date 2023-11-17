2ad4d069e80a82655227dee0412cc0b6477b5018
Ryan Francis <C21730391@mytudublin.ie>
2023-11-17 20:05:43 +0000

    New drawer and button

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.assignment
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.navigation.NavigationView
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // Initializing the map fragment
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        // Handle navigation item clicks
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_item1 -> startActivity(Intent(this, ActivityOne::class.java))
+                R.id.nav_item2 -> startActivity(Intent(this, ActivityTwo::class.java))
+                R.id.nav_item3 -> startActivity(Intent(this, ActivityThree::class.java))
+            }
+            // Close the drawer when an item is selected
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -48,4 +70,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // Set camera to show all markers
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(0.0, 0.0), 2f))
     }
+
+    // Function to handle drawer toggle button clicks
+    fun onDrawerToggle(view: View) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+}
+
+class ActivityThree {
+
+}
+
+class ActivityTwo {
+
+}
+
+class ActivityOne {
+
 }
